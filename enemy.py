82adbf2189068b61b07859b9dc0efe209e7c8faa
LiamH97 <88910099+LiamH97@users.noreply.github.com>
2024-01-03 11:17:31 +0000

    Added the actual stats
    Last update but added the stats for troll, undead and adjusted orc melee stats

--- enemy.py
+++ enemy.py
@@ -12,7 +12,7 @@ class Enemy():
         self.holy_resist = holy_resist
         self.hp = random.randrange(50,100) # enemy spawns with random health between 50 and 100
 
-# Creating the ORC class:
+## Creating the ORC class:
 # Increased damage taken from spell
 # Deals Melee damage
 # Reduced damage from melee
@@ -22,5 +22,27 @@ class Orc(Enemy):
     def __init__(self):
         super().__init__()
         self.spell_resist *= .9
-        self.meleedamage *= 1.1
+        self.meleedamage *= 1.3
         self.melee_resist *= 1.1
+
+## Creating the Troll class:
+# Increased damage taken from melee
+# Deals Melee damage - Face to face style fighting but has significant damage bonus from melee and reduced resistane to melee 
+# Reduced damage from spells
+# No change on holy damage
+class Troll(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.melee_resist *= .8
+        self.meleedamage *= 1.2
+        self.spell_resist *= 1.1
+
+## Defining Undead class
+#Undead are vunerable to holy, have higher spell resist, no impact on melee
+# Undead deal spell damage 
+class Undead(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.spelldamage *= 1.3
+        self.spell_resist *= 1.1
+        self.holy_resist *= .7
