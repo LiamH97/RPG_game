cb5e8448ef0b219b997f0da229b51146a4cabe97
LiamH97 <88910099+LiamH97@users.noreply.github.com>
2024-01-03 11:46:44 +0000

    Hero classes
    Created the hero classes, brought them into the hero file and used inheritance.
    Holy and Melee classes added.
    Left to add: Spell spec

--- hero.py
+++ hero.py
@@ -4,11 +4,30 @@
     # Available attacks and damage of each attack
     # EXP/Level - damage incrased by 5% per level
 class Hero():
-    # Hero base stats
-    Health = 300
-    holy_damage = 20
-    melee_damage = 20
-    spell_damage = 20
-    spell_resist = 10
-    melee_resist = 10
-    
+    def __init__(self, health = 300 ,holy_damage = 15,melee_damage = 20,spell_damage = 20,spell_resist = 10,melee_resist = 10):
+        self.health = health
+        self.holy_damage = holy_damage
+        self.melee_damage = melee_damage
+        self.spell_damage = spell_damage
+        self.spell_resist = spell_resist
+        self.melee_resist = melee_resist
+#Defining Holy class 
+# Increased holy damage
+class Holy(Hero):
+    def __init__(self,heal):
+        super().__init__()
+        self.holy_damage *= 1.5
+        self.heal = self.health * .1
+holy_heal = Holy(heal = 0) # add a cooldown of 3 rounds for this ability
+
+#Define Melee Class
+## Increased melee damage
+# Powerful melee ability
+class Melee(Hero):
+    def __init__(self,slash):
+        super().__init__()
+        self.melee_damage *= 1.5
+        self.melee_resist *= 1.1
+        self.slash = self.melee_damage * 1.5
+sweeping_slash = Melee(slash=0) # add a cooldown of 2 rounds for this ability
+print(sweeping_slash.slash)
--- holy-spec.py
+++
@@ -1,6 +0,0 @@
-# Child class of hero
-# Increased holy damage
-# Heal ability with CD of 3 turns
-from hero import Hero
-class Holy(Hero):
-    Hero.holy_damage = Hero.holy_damage * 1.1
