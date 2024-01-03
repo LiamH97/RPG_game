ad7fe3116f214afe5132cbb41fc897a8b05ec014
LiamH97 <88910099+LiamH97@users.noreply.github.com>
2024-01-03 12:18:38 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is where the main game takes place
+from hero import Hero
+from enemy import Enemy
+# Spec selection
+
+# Enemy encounters
+
+# Options to user
+
+# Keep record of health of Hero and Enemy
+
+# Keep record of when abilities were used for CD
+# Don't allow use of ability if still on CD
+
+# ------------------------------Test Encounter ------------------------------------#
+# Hero is Holy
+# Enemy is undead
